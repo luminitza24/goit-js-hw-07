d5d063ed66542be70df24bd80ee9c3868d9b347e
luminitza24 <cluminita24@yahoo.com>
2023-05-03 09:03:01 +0300

    adaugat librarii

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,19 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import * as basicLightbox from 'basiclightbox';
 for (let i = 0; i < galleryItems.length; i++){
     const newImage = document.createElement('img')
     newImage.classList.add( 'image-item')
-    newImage.setAttribute('source', galleryItems[i].original)
     newImage.src = galleryItems[i].preview;
-    newImage.setAttribute('data-source', "large-image.jpg");
+    newImage.setAttribute('data-source', galleryItems[i].original);
+    newImage.setAttribute('alt', "summer pictures");
     const link = document.createElement('a');
     link.appendChild(newImage);
-    link.setAttribute('href', "large-image.jpg");
-    link.changeAtribute('href', galleryItems[i])
+    link.setAttribute('href', galleryItems[i].original);
     link.classList.add("gallery__link");
     const listItem = document.createElement('li')
     listItem.appendChild(link);
     document.querySelector('.gallery').appendChild(listItem);
     listItem.classList.add("gallery-item");
+    link.addEventListener("click", function(event){
+      event.preventDefault()
+    });  
+  }
+
+  document.querySelector('a').onclick = () => {
+
+    basicLightbox.show('href')
   
   }
+  
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,12 +3,27 @@ import { galleryItems } from './gallery-items.js';
 for (let i = 0; i < galleryItems.length; i++){
     const newImage = document.createElement('img')
     newImage.classList.add( 'image-item')
-    newImage.setAttribute('source', galleryItems[i].original)
     newImage.src = galleryItems[i].preview;
+    newImage.setAttribute('data-source', galleryItems[i].original);
+    newImage.setAttribute('alt', "summer pictures");
+    const link = document.createElement('a');
+    link.appendChild(newImage);
+    link.setAttribute('href', galleryItems[i].original);
+    link.classList.add("gallery__link");
     const listItem = document.createElement('li')
-    listItem.appendChild(newImage);
-    document.querySelector('.gallery').appendChild(listItem)
-  
+    listItem.appendChild(link);
+    document.querySelector('.gallery').appendChild(listItem);
+    listItem.classList.add("gallery-item");
+    link.addEventListener("click", function(event){
+      event.preventDefault()
+    });  
   }
+  const lista = document.querySelector(".gallery");
+const myEvent= (e)=>{
+      const element = e.target;
+      const image = element.dataset.source;
+  }
+  lista.addEventListener("click", myEvent)
+  const lightbox = ('.gallery-items.js').simpleLightbox({ enableKeyboard:"esc", captionsData:"alt" });
+  lightbox.on(shown.simplelightbox, myEvent) 
   
-console.log(galleryItems);
